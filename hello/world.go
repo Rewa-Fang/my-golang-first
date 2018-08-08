c648570c71ebb7045f205d3e93531d07637c15fb
Rewa Fang <fangrh0627@163.com>
2018-08-08 22:56:58 +0800

    hello world

---
+++ hello/world.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+	fmt.Printf("Hello wolrd Rewa Fang.")
+	sum := 0;
+	for index:=0; index < 10 ; index++ {
+		sum += index
+	}
+	fmt.Println("sum is equal to ", sum)
+}
